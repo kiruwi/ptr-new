475c26a2243d1d7c3748cebd05d53c54118b0760
kiruwi <i4nkirui@gmail.com>
2026-03-11 11:54:12 +0300

    parallax case change fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
   srcDir: "src/",
-  css: ["~/assets/styles/Globals.css"],
+  css: ["~/assets/styles/globals.css"],
   devtools: { enabled: false },
   modules: [],
   app: {
